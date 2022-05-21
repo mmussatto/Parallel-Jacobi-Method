15a8d82f2fc8bf6f63f1c10d8f3e5f164b841050
Guerreiro51 <alessandrog54@usp.br>
2022-05-21 14:53:57 -0300

    Improved information displaying and code readability

--- jacobiseq.c
+++ jacobiseq.c
@@ -1,12 +1,64 @@
+// gcc jacobiseq.c -fopenmp -Wall -O3 -Ofast -march=native -std=c17 -o jacobiseq
 #include <math.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MIN_DEVIATION 1e-7
 #define MAX_ITERATIONS 100
 
+typedef struct jacobiRet {
+    double* solution;
+    int iterationsTaken;
+} JacobiRet;
+
 double randDouble() {
-    return rand() / (1.0 * RAND_MAX);
+    return rand() / (0.5 * RAND_MAX) - 1.0;
+}
+
+/** Generates a random A matrix such that Jacobi Method converges
+ * Note that a sufficient condition for convergence is that
+ * the matrix is diagonally dominant, i.e., for each row we garantee that
+ * the sum of the absolute value of every term in that row besides the diagonal one
+ * is less than the absolute value of the diagonal term.
+ *
+ * As such for row k, if we assume that rowSum is the sum of every term in that
+ * row besides the diagonal one, then rowSum/X = A[k][k] then dividing every
+ * element besides the diagonal one by X = rowSum/A[k][k] + c, where c is
+ * a positive constant transforms any matrix into a diagonally dominant matrix
+ */
+double** randomDiagonallyDominantMatrix(int matrixSize) {
+    // Allocates memory for the A matrix
+    double** matrix = (double**)malloc(sizeof(double*) * matrixSize);
+    for (int i = 0; i < matrixSize; i++) {
+        matrix[i] = (double*)malloc(sizeof(double) * matrixSize);
+    }
+
+    for (int i = 0; i < matrixSize; i++) {
+        matrix[i][i] = randDouble();
+        double rowSum = 0.0;
+        for (int j = 0; j < matrixSize; j++) {
+            if (i == j) continue;
+            matrix[i][j] = randDouble();
+            rowSum += fabs(matrix[i][j]);
+        }
+        double X = rowSum / fabs(matrix[i][i]) + fabs(randDouble());
+        for (int j = 0; j < matrixSize; j++) {
+            if (i == j) continue;
+            matrix[i][j] /= X;
+        }
+    }
+
+    return matrix;
+}
+
+double* randomVector(int vectorSize) {
+    // Creating a random B matrix
+    double* vector = (double*)malloc(sizeof(double) * vectorSize);
+    for (int i = 0; i < vectorSize; i++)
+        vector[i] = randDouble();
+
+    return vector;
 }
 
 void copyVector(double* dest, double* source, int vectorSize) {
@@ -23,15 +75,15 @@ double maxDiff(double* curr, double* prev, int vectorSize) {
     return maxDiff;
 }
 
-double* jacobiseq(double** A, double* B, int matrixSize) {
+JacobiRet jacobiseq(double** A, double* B, int matrixSize) {
+    int iterationCounter;
     double* prevX = (double*)malloc(sizeof(double) * matrixSize);
     double* currX = (double*)malloc(sizeof(double) * matrixSize);
     for (int i = 0; i < matrixSize; i++)
         prevX[i] = 0;
 
-    double deviation = 100;
-    int iterationCounter = 0;
-    for (int iterationCounter = 0; deviation >= MIN_DEVIATION && iterationCounter <= MAX_ITERATIONS; iterationCounter++) {
+    double deviation = INFINITY;
+    for (iterationCounter = 0; deviation >= MIN_DEVIATION && iterationCounter <= MAX_ITERATIONS; iterationCounter++) {
         for (int i = 0; i < matrixSize; i++) {
             double sum = 0.0;
             for (int j = 0; j < matrixSize; j++) {
@@ -44,8 +96,38 @@ double* jacobiseq(double** A, double* B, int matrixSize) {
         deviation = maxDiff(prevX, currX, matrixSize);
         copyVector(prevX, currX, matrixSize);
     }
+    free(prevX);
 
-    return currX;
+    JacobiRet ret = {currX, iterationCounter};
+    return ret;
+}
+
+void showLinearSystem(double** A, double* B, double* X, int matrixSize) {
+    for (int i = 0; i < matrixSize; i++) {
+        printf("[");
+        for (int j = 0; j < matrixSize - 1; j++) {
+            printf("%9.6lf ", A[i][j]);
+        }
+        if (i == (matrixSize - 1) / 2)
+            printf("%9.6lf] [%9.6lf]  =  [%9.6lf]\n", A[i][matrixSize - 1], X[i], B[i]);
+        else
+            printf("%9.6lf] [%9.6lf]     [%9.6lf]\n", A[i][matrixSize - 1], X[i], B[i]);
+    }
+}
+
+void showWolframAlphaInput(double** A, double* B, int matrixSize) {
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize - 1; j++) {
+            char letter = 'A' + j;
+            printf("(%lf * %c) + ", A[i][j], letter);
+        }
+        printf("(%lf * %c) = %lf", A[i][matrixSize - 1], 'A' + matrixSize - 1, B[i]);
+
+        if (i != matrixSize - 1)
+            printf(" and ");
+        else
+            printf("\n");
+    }
 }
 
 int main(int argc, char** args) {
@@ -58,56 +140,23 @@ int main(int argc, char** args) {
     srand(69420);
     int matrixSize = atoi(args[1]);
 
-    // Allocates memory for the A matrix
-    double** A = (double**)malloc(sizeof(double*) * matrixSize);
-    for (int i = 0; i < matrixSize; i++) {
-        A[i] = (double*)malloc(sizeof(double) * matrixSize);
-    }
-
-    /** Generates a random A matrix such that Jacobi Method converges
-     * Note that a sufficient condition for convergence is that
-     * the matrix is diagonally dominant, i.e., for each row we garantee that
-     * the sum of the absolute value of every term in that row besides the diagonal one
-     * is less than the absolute value of the diagonal term.
-     *
-     * Here, we are generating A matrices that every elements is positive. As such
-     * for row k, if we assume that rowSum is the sum of every term in that row
-     * besides the diagonal one, then rowSum/X = A[k][k] then dividing every
-     * element besides the diagonal one by X = rowSum/A[k][k] + c, where c is
-     * a positive constant transforms any matrix into a diagonally dominant matrix
-     */
-    for (int i = 0; i < matrixSize; i++) {
-        A[i][i] = randDouble();
-        double rowSum = 0.0;
-        for (int j = 0; j < matrixSize; j++) {
-            if (i == j) continue;
-            A[i][j] = randDouble();
-            rowSum += A[i][j];
-        }
-        double X = rowSum / A[i][i] + randDouble();
-        for (int j = 0; j < matrixSize; j++) {
-            if (i == j) continue;
-            A[i][j] /= X;
-        }
-    }
-
-    // Creating a random B matrix
-    double* B = (double*)malloc(sizeof(double) * matrixSize);
-    for (int i = 0; i < matrixSize; i++)
-        B[i] = (double)rand() / (1.0 * RAND_MAX);
+    double** A = randomDiagonallyDominantMatrix(matrixSize);
+    double* B = randomVector(matrixSize);
 
     // Determining the solution to the system of linear equations
-    double* X = jacobiseq(A, B, matrixSize);
+    double startTime = omp_get_wtime();
+    JacobiRet jacobiRet = jacobiseq(A, B, matrixSize);
+    double endTime = omp_get_wtime();
 
-    // Showing everything in a clean way
-    for (int i = 0; i < matrixSize; i++) {
-        printf("[");
-        for (int j = 0; j < matrixSize - 1; j++) {
-            printf("%lf ", A[i][j]);
-        }
-        if (i == (matrixSize - 1) / 2)
-            printf("%lf] [%lf]  =  [%lf]\n", A[i][matrixSize - 1], X[i], B[i]);
-        else
-            printf("%lf] [%lf]     [%lf]\n", A[i][matrixSize - 1], X[i], B[i]);
+    printf("Solved %dx%d linear system in %.3lf seconds after %d iterations\n", matrixSize, matrixSize, endTime - startTime, jacobiRet.iterationsTaken);
+    if (matrixSize <= 3) {
+        showLinearSystem(A, B, jacobiRet.solution, matrixSize);
+        showWolframAlphaInput(A, B, matrixSize);
     }
+
+    for (int i = 0; i < matrixSize; i++)
+        free(A[i]);
+    free(A);
+    free(B);
+    free(jacobiRet.solution);
 }
